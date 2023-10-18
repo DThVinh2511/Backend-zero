40906985fcaee653bd4c7f76725a787f7f6904ff
t.vinh25112003 <t.vinh25112003@gmail.com>
2023-10-19 01:04:03 +0700

    ud34555

--- src/config/database.js
+++ src/config/database.js
@@ -41,9 +41,9 @@ const dbState = [{
 ];
 const connection = async () => {
   const options = {
-    user: "root",
+    user: process.env.DB_USER,
     pass: process.env.DB_PASSWORD,
-    dbName: "dothanhvinh"
+    dbName: process.env.DB_NAME
   }
   await mongoose.connect(process.env.DB_HOST, options);
   const state = Number(mongoose.connection.readyState);
